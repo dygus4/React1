9aa47f336c10924673b360cb582389131f361302
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-05-15 17:03:04 +0200

    Add Column

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,17 @@
 
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{props.titleText}</h2>
-    <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
+    <img className={styles.image} src={props.heroImage}></img>
   </header>
 );
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+    heroImage: PropTypes.string,
+};
 
 export default Hero;
