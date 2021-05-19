29e298d776a38a06f696c244f4e2b247925390dc
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-05-19 23:40:12 +0200

    16.3

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,14 +3,12 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import {settings} from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 
 class Column extends React.Component {
-    state = {
-      cards: this.props.cards || [],
-    }
+    
     static propTypes = {
       title: PropTypes.string.isRequired,   
       icon: PropTypes.node,
@@ -19,38 +17,27 @@ class Column extends React.Component {
     static defaultProps = {
       description: settings.defaultColumnIcon,
     }
-    addCard(title){
-      this.setState(state => (
-        {
-          cards: [
-            ...state.cards,
-            {
-              key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-              title,
-            },
-          ],
-        }
-      ));
-    }
-
-
+    
     render() {
+      const {title, icon, cards} = this.props;
       return (
         <section className={styles.component}>
           <h3 className={styles.title}>
-            {this.props.title}
+            {title}
             <span className={styles.icon}>
-              <Icon name={this.props.icon}></Icon>
+              <Icon name={icon}></Icon>
             </span>
           </h3>
           <div>
-            {this.state.cards.map(({key, ...cardProps}) => (
+            {cards.map(({key, ...cardProps}) => (
               <Card key={key} {...cardProps} />
             ))}
           </div>
+          {/*
           <div className={styles.creator}>
             <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
           </div>
+            */}
         </section>
       );
     }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+import Column from '../Column/ColumnContainer.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
+  //state = {
+  //  columns: this.props.columns || [],
+  //}
     static propTypes = {
       title: PropTypes.node.isRequired,
       image: PropTypes.string,
@@ -21,7 +21,7 @@ class List extends React.Component {
     static defaultProps = {
       description: settings.defaultListDescription,
     }
-    addColumn(title){
+    /*addColumn(title){
       this.setState(state => (
         {
           columns: [
@@ -35,27 +35,28 @@ class List extends React.Component {
           ],
         }
       ));
-    }
+    }*/
     render() {
+      const {title, image, description, columns} = this.props;
       return (
         <section className={styles.component}>
 
-          <Hero titleText={this.props.title} heroImage={this.props.image}/>
+          <Hero titleText={title} heroImage={image}/>
 
           <div className={styles.description}>
-            {ReactHtmlParser(this.props.description)}
+            {ReactHtmlParser(description)}
           </div>
-
+          
           <div className={styles.columns}>
-            {this.state.columns.map(({key, ...columnProps}) => (
-              <Column key={key} {...columnProps} />
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
             ))}
           </div>
-
+          {/*
           <div className={styles.creator}>
             <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>
-
+            */}
         </section>
       );
     }
